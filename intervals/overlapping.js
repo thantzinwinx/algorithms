625ace6f7a14e2891687696f07ec0f22665bda32
Thant Zin Win <ln.thantzinwin@gmail.com>
2026-02-11 22:54:38 +0630

    practice interval meetings problem

---
+++ intervals/overlapping.js
@@ -0,0 +1,15 @@
+function canAttendMeetings(intervals) {
+  if (intervals.length === 0) {
+    return true;
+  }
+
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] < intervals[i - 1][1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
