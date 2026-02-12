62b808c8c17f242c5d884423027b42b541ef1447
Thant Zin Win <ln.thantzinwin@gmail.com>
2026-02-12 15:40:50 +0630

    feat: add merge intervals algorithm practice

---
+++ intervals/merge-interval.js
@@ -0,0 +1,31 @@
+const intervals = [
+  [1, 5],
+  [3, 6],
+  [8, 10],
+  [15, 18],
+];
+
+function mergeIntervals(intervals) {
+  //sort the intervals
+  const sortedIntervals = intervals.sort((a, b) => a[1] - b[1]);
+
+  //create state for the final merged intervals
+  const mergedIntervals = [];
+
+  //iterate the intervals
+  for (const interval of sortedIntervals) {
+    if (
+      mergedIntervals.length === 0 ||
+      interval[0] > mergedIntervals[mergedIntervals.length - 1][1]
+    ) {
+      mergedIntervals.push(interval);
+    }
+    mergedIntervals[mergedIntervals.length - 1][1] = Math.max(
+      interval[1],
+      mergedIntervals[mergedIntervals.length - 1][1],
+    );
+  }
+  return mergedIntervals;
+}
+
+console.log("Merged Interval", mergeIntervals(intervals));
