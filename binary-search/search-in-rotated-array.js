5d342e0366d4f5f77ba7ff826012e222ddd8da67
Thant Zin Win <ln.thantzinwin@gmail.com>
2026-04-05 12:36:10 +0630

    feat: solve search in rotated sorted array using binary search

---
+++ binary-search/search-in-rotated-array.js
@@ -0,0 +1,31 @@
+const nums = [8, 9, 10, 12, 16, 17, 1, 2, 3];
+const target = 3;
+
+const searchInRotated = (nums, target) => {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
+
+console.log("Search In Rotated", searchInRotated(nums, target));
